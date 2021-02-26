5cca038b1378dd5f5b51aeba39c185ea7117ade5
Jeyakiruba21 <jeyakirubakaran21@gmail.com>
2021-02-26 15:42:18 +0530

    mainpage+login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Signup from './components/Signup';
+import './App.css';
 import {Container} from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
+
 function App() {
   return (
-    
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh" }}> 
-<div className="div w-100" style={{maxWidth:"400px"}}>
+    <>   
+    <Container className="d-flex align-items-center justify-content-center" style={{maxHeight: "100vh" }}> 
+<div className="card div h-100 w-100" style={{maxWidth:"500px"}}>
 <Router>
   <AuthProvider>
 <Switch>
@@ -21,7 +23,7 @@ function App() {
 </Router> 
           </div>
     </Container>
-    
+    </>
     
   );
 }
